373760e48029ffdf34c3a29714df8342cd6bbc5e
u8slvn <u8slvn@gmail.com>
2023-07-30 20:23:19 +0200

    feat: add auto font loading

--- until_zero/__init__.py
+++ until_zero/__init__.py
@@ -1,11 +1,6 @@
 from __future__ import annotations
 
-from importlib import metadata
-
 from until_zero.app import App
 
 
-__app_name__ = "until_zero"
-__version__ = metadata.version(__app_name__)
-
-__all__ = ["__app_name__", "__version__", "App"]
+__all__ = ["App"]
