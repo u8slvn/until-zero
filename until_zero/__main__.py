fe3fcbc6bcbc078a0ed203314c41d112e5802456
u8slvn <u8slvn@gmail.com>
2023-07-30 17:14:18 +0200

    feat: add splash screen

--- until_zero/__main__.py
+++ until_zero/__main__.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
 from until_zero import App
+from until_zero.constants import COMPILED_ENV
+
+
+if COMPILED_ENV:
+    import pyi_splash
 
 
 if __name__ == "__main__":
     app = App()
+
+    if COMPILED_ENV:
+        pyi_splash.close()
+
     app.mainloop()
